b0ef23a5c7e7a982a2e8da1667142abd03a559e9
Tomek Romaniuk <tomasz.romaniuk@gmail.com>
2016-01-30 23:05:28 +0100

    Remove bootstrap code

--- app.fsx
+++ app.fsx
@@ -1,13 +1,3 @@
-#if BOOTSTRAP
-System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-if not (System.IO.File.Exists "paket.exe") then let url = "https://github.com/fsprojects/Paket/releases/download/2.40.9/paket.exe" in use wc = new System.Net.WebClient() in let tmp = System.IO.Path.GetTempFileName() in wc.DownloadFile(url, tmp); System.IO.File.Move(tmp,System.IO.Path.GetFileName url);;
-#r "paket.exe"
-Paket.Dependencies.Install (System.IO.File.ReadAllText "paket.dependencies")
-#endif
-
-//---------------------------------------------------------------------
-
-#I "packages/Suave/lib/net40"
 #r "packages/Suave/lib/net40/Suave.dll"
 
 open System
@@ -27,7 +17,7 @@ let config =
         bindings=[ (if port = null then HttpBinding.mk HTTP ip127 (uint16 8080)
                     else HttpBinding.mk HTTP ipZero (uint16 port)) ] }
 
-startWebServer config (Successful.OK "Hello World!")
+startWebServer config (Successful.OK "Hello from Sauve!")
 printfn "exiting server..."
 
 
